3ca22d3c770d863ba803571ff22e5bc75c03ae92
dscholz-tgm <dscholz@student.tgm.ac.at>
2014-02-11 22:06:13 +0100

    MessageReader zum lesen des Topics

--- src/jmschat/ChatClient.java
+++ src/jmschat/ChatClient.java
@@ -2,15 +2,23 @@ package jmschat;
 
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
 import ui.InputReader;
 import ui.Display;
 import jmschat.utils.ANSIColor;
 import jmschat.utils.TextReader;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
 
 /**
  * Der Client
  * @author Dominik
- * @version 0.4
+ * @version 0.5
  */
 public class ChatClient {
     
@@ -22,6 +30,11 @@ public class ChatClient {
     private Display display = new Display();
     private ChatController controller = new ChatController();
     private MessageConstructor mc;
+    
+    Session session = null;
+    Connection connection = null;
+    MessageConsumer consumer = null;
+    Destination destination = null;
 
     /**
      * Konstruktor
@@ -38,6 +51,51 @@ public class ChatClient {
             ip = "127.0.0.1";
         }
         mc = new MessageConstructor(username, ip);
+    }
+        
+    /**
+     * Startet den Client
+     */
+    public void start() {
+        display.inlineOut(ANSIColor.CYAN);
+        display.out(TextReader.read(TextReader.FILE_WELCOME));
+        display.inlineOut(ANSIColor.GREEN);
+        display.out(TextReader.read(TextReader.FILE_HELP));
+        display.inlineOut(ANSIColor.RESET);
+        new Thread(new InputReader(this)).start();
+        connect();
+        createChatroom();
+        new Thread(new MessageReader(this,consumer)).start();
+    }
+       
+    /**
+     * Verbinden und Erstellen der Connection
+     */
+    public void connect() {
+        try {
+            close();
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(username + ip, ActiveMQConnection.DEFAULT_PASSWORD, url);
+            connection = connectionFactory.createConnection();
+            connection.start();
+            
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        } catch (JMSException ex) {
+            err("Fehler beim Verbinden zum Server " + url + ", bitte versuche es erneut");
+        }
+    }
+    
+    /**
+     * Erstellt den Chatroom oder tritt ihm bei
+     */
+    public void createChatroom() {
+        try {
+            if(consumer != null) consumer.close();
+            destination = session.createTopic(chatroom);
+            
+            consumer = session.createConsumer(destination);
+        } catch (JMSException ex) {
+            err("Fehler beim Erstellen des Chatrooms " + chatroom + ", bitte versuche es erneut");
+        }
     }
     
     /**
@@ -79,18 +137,6 @@ public class ChatClient {
     private void changeChatroom(String chatroom) {
         this.chatroom = chatroom;
     }
-    
-    /**
-     * Startet den Client
-     */
-    public void start() {
-        display.inlineOut(ANSIColor.CYAN);
-        display.out(TextReader.read(TextReader.FILE_WELCOME));
-        display.inlineOut(ANSIColor.GREEN);
-        display.out(TextReader.read(TextReader.FILE_HELP));
-        display.inlineOut(ANSIColor.RESET);
-        new Thread(new InputReader(this)).start();
-    }
 
     /**
      * Gibt eine Nachricht aus
@@ -116,5 +162,17 @@ public class ChatClient {
     public void promt() {
         display.inlineOut(ANSIColor.DARK_GRAY);
         display.inlineOut(mc.construct(""));
+    }
+
+    /**
+     * Schliesst alle Verbindungen
+     */
+    private void close() {
+        try {
+            if(consumer != null) consumer.close();
+            if(session != null) session.close();
+            if(connection != null) connection.close();
+        } catch (JMSException ex) {
+        }
     }
 }
---
+++ src/jmschat/MessageReader.java
@@ -0,0 +1,45 @@
+package jmschat;
+
+import jmschat.utils.Stoppable;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.TextMessage;
+
+/**
+ * Reads and Reads and Reads
+ *
+ * @author Dominik
+ * @version 0.1
+ */
+public class MessageReader implements Runnable, Stoppable {
+
+    private ChatClient cc;
+    private MessageConsumer consumer;
+    private boolean stop = false;
+
+    public MessageReader(ChatClient cc, MessageConsumer consumer) {
+        this.cc = cc;
+    }
+
+    @Override
+    public void stop() {
+        stop = true;
+    }
+
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                Message message = consumer.receive();
+                if (message instanceof TextMessage && message != null) {
+                    TextMessage textMessage = (TextMessage) message;
+                    cc.out(textMessage.getText());
+                    message.acknowledge();
+                }
+            } catch (JMSException ex) {
+            }
+        }
+    }
+
+}
